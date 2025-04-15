e03725254fcdf0ea5fd541a70703bff308285fb7
lokeshBellurbis <lokesh.singh@bellurbis.com>
2025-04-15 15:26:00 +0530

    [custom api N  error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
     path:"./env"
 })
 
-
-connectDB()
+const PORT = process.env.PORT || 8000
+connectDB().then(() =>{
+   
+    app.on("error" ,(error) =>{
+        console.log('ERR',error);
+        throw error
+    } )
+    app.listen(PORT,() => console.log(`Server is running at port :${PORT}`))
+}).catch((err) =>{
+    console.log("MONGO DB connection failed !!! " , err)
+})
